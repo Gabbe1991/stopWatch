031deafa7fb366e318227ce018f2350265170c67
Gabriel <gabe.jara@gmail.com>
2022-11-11 15:51:04 +0100

    AppStopWatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [time, setTime ] = React.useState(0)
+    const [timerOn, setTimerOn] = React.useState(false)
+    const [timerStart, setTimerStart] = React.useState(0)
+    const [timerTime, setTimerTime] = React.useState(0)
 
-export default App;
+    React.useEffect(() => {
+        let interval = null
+        if (timerOn) {
+            interval = setInterval(() => {
+                const newTime = Date.now() - timerStart
+                setTime(newTime)
+            }, 10)
+        } else if (!timerOn) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval)
+    }, [timerOn])
+
+    const handleStart = () => {
+        setTimerOn(true)
+        setTimerTime(time)
+        setTimerStart(Date.now() - time)
+    }
+
+    const handleStop = () => {
+        setTimerOn(false)
+    }
+
+    const handleReset = () => {
+        setTimerOn(false)
+        setTime(0)
+        setTimerTime(0)
+    }
+
+    const handleResume = () => {
+        setTimerOn(true)
+        setTimerStart(Date.now() - timerTime)
+    }
+
+
+
+    return (
+        <div className="App">
+            <div>{time}</div>
+            <div>
+                <button onClick={() => handleStart()}>Start</button>
+                <button onClick={() => handleStop()}>Stop</button>
+                <button onClick={() => handleReset()}>Reset</button>
+
+
+            </div>
+
+        </div>
+    );
+    
+}
---
+++ src/backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import User from '../models/usersModel.js'
+const connectDB = async () => {
+    try {
+        //database Name
+        const databaseName='stopwatch'; 
+        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+       
+        
+    });
+    
+        console.log(`Database connected : ${con.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+
+}
+
+export default connectDB
